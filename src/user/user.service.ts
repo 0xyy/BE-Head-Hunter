d5439b58be7589b82d944084cf9c33056842265d
Marcin Bilka <bilkadev@gmail.com>
2022-07-22 11:15:34 +0200

    Add active methode to UserService.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { EditPasswordDto } from './dto/edit-password.dto';
 import { RecoverPasswordDto } from './dto/recover-password.dto';
 import { ActivateUserDto } from './dto/activate-user.dto';
+import { User } from './user.entity';
+import { ActivateUserResponse } from '../types';
+import { hashPwd } from '../utils/hash-pwd';
 
 @Injectable()
 export class UserService {
@@ -13,7 +16,24 @@ export class UserService {
     return Promise.resolve(undefined);
   }
 
-  async activate(active: ActivateUserDto) {
-    return Promise.resolve(undefined);
+  async activate(active: ActivateUserDto): Promise<ActivateUserResponse> {
+    const { userId, token, password } = active;
+    const user = await User.findOne({
+      where: {
+        id: userId,
+        active: false,
+        activeTokenId: token,
+      },
+    });
+    if (!user) {
+      return { isSuccess: false };
+    }
+    user.pwdHash = hashPwd(password);
+    user.active = true;
+    user.activeTokenId = null;
+    await user.save();
+    return {
+      isSuccess: true,
+    };
   }
 }
