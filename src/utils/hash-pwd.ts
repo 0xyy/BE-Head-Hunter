dea10f601a34cddfdaa959595ba711d6616deb0a
Marcin Bilka <bilkadev@gmail.com>
2022-07-23 00:07:13 +0200

    Add randomSalz function.

--- src/utils/hash-pwd.ts
+++ src/utils/hash-pwd.ts
@@ -1,10 +1,11 @@
 import * as crypto from 'crypto';
 
-const SALZ =
-  'nkjsdafh982y3 98h qoi232ny9823ync!@#982y1ocrixmh29yy398z2y4982y3z,k2!$%!$#%!#%io h239i432hi423h423iu ec2 e3 3żśćśąąasdfs@%@#%afdsadfsadfsadfo9as09du09io';
-
-export const hashPwd = (p: string): string => {
-  const hmac = crypto.createHmac('sha512', SALZ);
+export const hashPwd = (p: string, salz: string): string => {
+  const hmac = crypto.createHmac('sha512', salz);
   hmac.update(p);
   return hmac.digest('hex');
+};
+export const randomSalz = (size: number) => {
+  const salz = crypto.randomBytes(size);
+  return salz.toString();
 };
