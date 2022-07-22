59c0eb9d2151ff710e9a7e9872dcccf180a721cb
Marcin Bilka <bilkadev@gmail.com>
2022-07-22 09:49:29 +0200

    Add UserInterface to Types. and change in UserRole coursant to student. Change import RecoverDto to ReccoverPasswordDto.

--- src/types/user/user.ts
+++ src/types/user/user.ts
@@ -1,6 +1,16 @@
 import { ActivateUserDto } from '../../user/dto/activate-user.dto';
 import { EditPasswordDto } from '../../user/dto/edit-password.dto';
-import { RecoverDto } from '../../user/dto/recover-password.dto';
+import { RecoverPasswordDto } from '../../user/dto/recover-password.dto';
+
+export interface UserInterface {
+  id: string;
+  email: string;
+  pwdHash: string;
+  currentTokenId: string | null;
+  activeTokenId: string | null;
+  active: boolean;
+  role: UserRole;
+}
 
 export type EditPasswordResponse = {
   isSuccess: boolean;
@@ -13,10 +23,10 @@ export type RecoverPasswordResponse = {
 };
 export enum UserRole {
   ADMIN,
-  COURSANT,
+  STUDENT,
   HR,
 }
 
 export type ActivateUserRequest = ActivateUserDto;
 export type EditPasswordRequest = EditPasswordDto;
-export type RecoverPasswordRequest = RecoverDto;
+export type RecoverPasswordRequest = RecoverPasswordDto;
