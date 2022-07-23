a21f49f5d8d4aa389409ccc6bb397b5fe8a4ea5d
kamil <you@example.com>
2022-07-23 18:08:48 +0200

    admin modules created

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Inject, Patch} from '@nestjs/common';
+import {AdminService} from "./admin.service";
+import {RecoverPasswordDto} from "./dto/recoverPasswordDto";
+import {EditPasswordDto} from "./dto/editPasswordDto";
+import {
+    EditPasswordResponse,
+    RecoverPasswordResponse,
+} from '../types';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(@Inject(AdminService) private userService: AdminService) {}
+
+    @Get('/allCoursants')
+    getAllCoursants(): Promise<CoursantEntity[]> {
+        return this.coursantService.getAll()
+    }
+
+    @Get('/allHR')
+    getAllHR(): Promise<HREntity[]> {
+        return this.hrService.getAll()
+    }
+
+    //import all coursant from file
+
+    //add new hr
+
+    @Patch(`/edit`)
+    editPassword(
+        @Body() password: EditPasswordDto,
+    ): Promise<EditPasswordResponse> {
+        return this.userService.editPassword(password);
+    }
+
+    @Get(`/recover`)
+    recoverPassword(
+        @Body() recover: RecoverPasswordDto,
+    ): Promise<RecoverPasswordResponse> {
+        return this.userService.recover(recover);
+    }
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import {EditPasswordDto} from "./dto/editPasswordDto";
+import {RecoverPasswordDto} from "./dto/recoverPasswordDto";
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    async editPassword(password: EditPasswordDto) {
+        return Promise.resolve(undefined);
+    }
+
+    async recover(recover: RecoverPasswordDto) {
+        return Promise.resolve(undefined);
+    }
+
+}
