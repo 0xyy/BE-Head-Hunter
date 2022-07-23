1743738173270285e5604db95a4f3fa4e22fae63
kamil <you@example.com>
2022-07-23 19:44:51 +0200

    config updated

--- src/mailerconfig.ts
+++ src/mailerconfig.ts
@@ -2,7 +2,7 @@
 //konfiguracja maila
 export = {
   // nazwa uzytkownika: haslo @ host : port
-  transport: `smtp://headhunter:123456@mail.bilka.networkmanager.pl:587`,
+  transport: `smtp://headhunter@bilka.networkmanager.pl:123456@mail.bilka.networkmanager.pl:587`,
   defaults: {
     from: 'headhunter@bilka.networkmanager.pl', //od kogo
   },
