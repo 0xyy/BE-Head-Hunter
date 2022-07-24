d93df8238e226a05bea2d3d7eef0c05526b89300
kamil <you@example.com>
2022-07-24 15:27:22 +0200

    admin module updated

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { UserModule } from '../user/user.module';
+import { HrModule } from '../hr/hr.module';
+import { StudentModule } from '../student/student.module';
 
 @Module({
-    controllers: [AdminController],
-    providers: [AdminService]
+  imports: [UserModule, HrModule, StudentModule],
+  controllers: [AdminController],
+  providers: [AdminService],
 })
 export class AdminModule {}
