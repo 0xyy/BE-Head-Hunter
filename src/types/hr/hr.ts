665e62fb0061c2a7fdcc46126b29d5c6003e4996
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-26 15:41:15 +0200

    Added types for create Hr request and response.

--- src/types/hr/hr.ts
+++ src/types/hr/hr.ts
@@ -1,12 +1,19 @@
 import { StudentProjectUrlInterface } from '../student';
 import { UserInterface } from '../user';
+import { CreateHrDto } from '../../hr/dto/create-hr.dto';
 
 export interface HrInterface {
   id: string;
-  email: string;
   fullName: string;
   company: string;
   maxReservedStudents: number;
   studentsToInterview: StudentProjectUrlInterface[];
   user: UserInterface;
 }
+
+export type CreateHrRequest = CreateHrDto;
+
+export type CreateHrResponse = {
+  isSuccess: true;
+  message: string;
+};
