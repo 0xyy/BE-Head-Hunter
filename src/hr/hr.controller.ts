c0dbf8623aad12c0520ee9fc61df926f5534a762
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-26 15:46:04 +0200

    Ending method createHr.

--- src/hr/hr.controller.ts
+++ src/hr/hr.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { HrService } from './hr.service';
+import { CreateHrRequest, CreateHrResponse } from '../types/hr';
 
 // TODO te EP beda uzywac serwisy innych modulow
 
 @Controller('/hr')
 export class HrController {
-  constructor(
-      private readonly hrService: HrService
-  ) {}
+  constructor(private readonly hrService: HrService) {}
 
   @Get('/students/available')
   showAvailableStudents(): Promise<void> {
@@ -20,13 +19,13 @@ export class HrController {
   }
 
   @Get('/student/:id')
-  showSingleStudent(
-      @Param('id') id: string,
-  ): Promise<void> {
+  showSingleStudent(@Param('id') id: string): Promise<void> {
     // pobranie pojedynczego kursanta, użycie innego serwisu
     return;
   }
 
-
-
+  @Post('/test/')
+  async test(@Body() hr: CreateHrRequest): Promise<CreateHrResponse> {
+    return this.hrService.createHr(hr);
+  }
 }
