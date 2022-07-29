0c97577558d1b39b088bfcb5e18485b801429869
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-29 18:48:05 +0200

    Add InserStudentResponse type.

--- src/types/admin/admin.ts
+++ src/types/admin/admin.ts
@@ -1,7 +1,17 @@
 import { CreateHrDto } from '../../admin/dto/create-hr.dto';
+import { UserInterface } from '../user';
 
 export type CreateHrRequest = CreateHrDto;
 
 export type CreateHrResponse =
   | { message: string; isSuccess: false }
   | { isSuccess: true };
+
+export type InsertStudentResponse =
+  | {
+      isSuccess: true;
+    }
+  | {
+      user: UserInterface;
+      errors: string[];
+    }[];
