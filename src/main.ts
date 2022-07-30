4133c15e03666ef609ddefb81cc9764056b36fc0
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-30 13:19:49 +0200

    Change cors.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ import { GlobalExceptionFilter } from './filters/global-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
