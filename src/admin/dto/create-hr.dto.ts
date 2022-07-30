564ef8c4d1e32478737c4d5bbbe4b8c57aedea87
Tommy Phan <tommyphan2004.tp@gmail.com>
2022-07-30 17:29:29 +0200

    Code refactoring and optimize imports for admin module.

--- src/admin/dto/create-hr.dto.ts
+++ src/admin/dto/create-hr.dto.ts
@@ -1,21 +1,21 @@
 import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
 
 export class CreateHrDto {
-  @IsNotEmpty()
-  @IsString()
-  email: string;
+    @IsNotEmpty()
+    @IsString()
+    email: string;
 
-  @IsNotEmpty()
-  @IsString()
-  fullName: string;
+    @IsNotEmpty()
+    @IsString()
+    fullName: string;
 
-  @IsNotEmpty()
-  @IsString()
-  company: string;
+    @IsNotEmpty()
+    @IsString()
+    company: string;
 
-  @IsNotEmpty()
-  @IsInt()
-  @Min(1)
-  @Max(999)
-  maxReservedStudents: number;
+    @IsNotEmpty()
+    @IsInt()
+    @Min(1)
+    @Max(999)
+    maxReservedStudents: number;
 }
