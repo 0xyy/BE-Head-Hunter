670f4c86c5abafc7bbaa91afb6b275f2b9c22e42
Tommy <tommyphan2004.tp@gmail.com>
2022-08-01 19:10:56 +0200

    Code refactoring and optimize imports for types.

--- src/types/admin/admin.ts
+++ src/types/admin/admin.ts
@@ -4,20 +4,20 @@ import { UserInterface } from '../user';
 export type CreateHrRequest = CreateHrDto;
 
 export type CreateHrResponse =
-  | { message: string; isSuccess: false }
-  | { isSuccess: true };
+    | { message: string; isSuccess: false }
+    | { isSuccess: true };
 
 export type InsertStudentResponse =
-  | {
-      countSuccess: number;
-      isSuccess: true;
+    | {
+        countSuccess: number;
+        isSuccess: true;
     }
-  | {
-      isSuccess: false;
-      users: {
-        user: UserInterface;
-        errors: string[];
-      }[];
-      countSuccess: number;
-      countFailed: number;
+    | {
+        isSuccess: false;
+        users: {
+            user: UserInterface;
+            errors: string[];
+        }[];
+        countSuccess: number;
+        countFailed: number;
     };
--- src/types/hr/hr.ts
+++ src/types/hr/hr.ts
@@ -2,24 +2,24 @@ import { StudentProjectUrlInterface } from '../student';
 import { UserInterface } from '../user';
 
 export interface HrInterface {
-  id: string;
-  fullName: string;
-  company: string;
-  maxReservedStudents: number;
-  studentsToInterview: StudentProjectUrlInterface[];
-  user: UserInterface;
+    id: string;
+    fullName: string;
+    company: string;
+    maxReservedStudents: number;
+    studentsToInterview: StudentProjectUrlInterface[];
+    user: UserInterface;
 }
 
 export type AdminInsertHr = {
-  token: string;
-  email: string;
-  fullName: string;
-  company: string;
-  maxReservedStudents: number;
+    token: string;
+    email: string;
+    fullName: string;
+    company: string;
+    maxReservedStudents: number;
 };
 
 export type AdminCreateHrResponse = {
-  isSuccess: true;
-  userId: string;
-  message: string;
+    isSuccess: true;
+    userId: string;
+    message: string;
 };
