4686b1bd3717c5fe9b436864b2aa0f1a64af467c
Tommy <tommyphan2004.tp@gmail.com>
2022-08-01 19:16:23 +0200

    Code refactoring and optimize imports for app module files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { HrModule } from './hr/hr.module';
 import { DatabaseModule } from './database/database.module';
 import { StudentModule } from './student/student.module';
-import { UserModule } from './user/user.module';
-import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    AuthModule,
-    AdminModule,
-    UserModule,
-    HrModule,
-    StudentModule,
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 10,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+    imports: [
+        DatabaseModule,
+        AuthModule,
+        AdminModule,
+        UserModule,
+        HrModule,
+        StudentModule,
+        ThrottlerModule.forRoot({
+            ttl: 60,
+            limit: 10,
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule {}
