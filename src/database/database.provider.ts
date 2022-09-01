5ff87ed2065330ee613a2468795c9f54c78a4525
Jakub Król <jakubkrol@o2.pl>
2022-09-01 16:53:45 +0200

    Empty

--- src/database/database.provider.ts
+++
@@ -1,8 +0,0 @@
-import { dataSource } from '../config/config-database';
-
-export const databaseProviders = [
-  {
-    provide: 'DATA_SOURCE',
-    useFactory: async () => dataSource.initialize(),
-  },
-];
